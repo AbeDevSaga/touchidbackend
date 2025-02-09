05964a7749d8d1758caa532ff2d0e2843611752b
AbeDevSaga <2000amandanm@gmail.com>
2025-02-09 22:07:45 +0300

    Fix dependencies and clean up

--- admin/seedAdmin.js
+++ admin/seedAdmin.js
@@ -1,5 +1,5 @@
 const Admin = require("../models/Admin");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 
 const seedAdmin = async () => {
   try {
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const Admin = require("../models/Admin");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const User = require("../models/User");
 
 const router = express.Router();
